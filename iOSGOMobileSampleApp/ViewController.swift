9f1a06f52b2fc00181cb8b5ec1339abb7be56e29
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-05-24 10:43:24 -0500

    Create xcode project

---
+++ iOSGOMobileSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSGOMobileSampleApp
+//
+//  Created by Adriana Pineda on 5/24/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
